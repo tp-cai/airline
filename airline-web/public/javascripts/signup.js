270dc8ec5c46037a5618529ed68356e775712990
tp-cai <tempayc@gmail.com>
2023-07-14 11:51:14 -0400

    debug

--- airline-web/public/javascripts/signup.js
+++ airline-web/public/javascripts/signup.js
@@ -38,13 +38,13 @@ function selectProfile(profileId, profileDiv) {
 
 
 function signup(form) {
-	// grecaptcha.ready(function() {
-	// 	grecaptcha.execute('6LespV8UAAAAAJkCUpR8_uNC3P-wZGq7vnTNKEZe', {action: 'signup'})
-	// 		.then(function(token) {
-	//			form.append('<input type="hidden" name="recaptchaToken" value="' + token + '" />');
+	grecaptcha.ready(function() {
+		grecaptcha.execute('6LespV8UAAAAAJkCUpR8_uNC3P-wZGq7vnTNKEZe', {action: 'signup'})
+			.then(function(token) {
+				form.append('<input type="hidden" name="recaptchaToken" value="' + token + '" />');
 				form.submit()
-		// 	});
-		// });
+			});
+		});
 }
 
 	
